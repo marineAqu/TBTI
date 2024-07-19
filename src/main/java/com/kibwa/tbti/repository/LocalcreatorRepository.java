fb0e3f4439641f9557de291753202fac9844948c
marineAqu <102001809+marineAqu@users.noreply.github.com>
2024-07-19 19:16:03 +0900

    [feat] LocalcreatorRepository 생성 (2024.7.19 김도연)

---
+++ src/main/java/com/kibwa/tbti/repository/LocalcreatorRepository.java
@@ -0,0 +1,11 @@
+package com.kibwa.tbti.repository;
+import com.kibwa.tbti.entity.LocalcreatorEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * 파일명: localcreatorRepository
+ * 작성자: 김도연
+ **/
+public interface LocalcreatorRepository extends JpaRepository<LocalcreatorEntity, Integer> {
+    LocalcreatorEntity findByStore_id(int store_id);
+}
