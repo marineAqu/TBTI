8d58387c24dbb0a244f5c5618f94b86bcd72feeb
marineAqu <102001809+marineAqu@users.noreply.github.com>
2024-07-19 19:15:28 +0900

    [feat] LocalcreatorDetailService 생성 (2024.7.19 김도연)

---
+++ src/main/java/com/kibwa/tbti/service/LocalcreatorDetailService.java
@@ -0,0 +1,23 @@
+package com.kibwa.tbti.service;
+
+import com.kibwa.tbti.DTO.LocalcreatorDTO;
+import com.kibwa.tbti.entity.LocalcreatorEntity;
+import com.kibwa.tbti.repository.LocalcreatorRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class LocalcreatorDetailService {
+    private final LocalcreatorRepository localcreatorRepository;
+
+    public LocalcreatorDTO SearchByStoreId(int store_id) {
+        LocalcreatorEntity localcreatorEntity = localcreatorRepository.findByStore_id(store_id);
+
+        LocalcreatorDTO localcreatorDTO = LocalcreatorDTO.toLocalcreatorDTO(localcreatorEntity);
+
+        return localcreatorDTO;
+    }
+}
