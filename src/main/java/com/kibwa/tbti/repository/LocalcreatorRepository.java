eee0f573ddf085768e8f312fb12c81efaa03118f
marineAqu <102001809+marineAqu@users.noreply.github.com>
2024-07-21 11:34:51 +0900

    [feat] findByStoreNameLike 메소드 추가 (2024.7.21 김도연)

--- src/main/java/com/kibwa/tbti/repository/LocalcreatorRepository.java
+++ src/main/java/com/kibwa/tbti/repository/LocalcreatorRepository.java
@@ -1,11 +1,17 @@
 package com.kibwa.tbti.repository;
 import com.kibwa.tbti.entity.LocalcreatorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 /**
  * 파일명: localcreatorRepository
  * 작성자: 김도연
  **/
+
 public interface LocalcreatorRepository extends JpaRepository<LocalcreatorEntity, Integer> {
     LocalcreatorEntity findByStoreId(int store_id);
+
+    List<LocalcreatorEntity> findByStoreNameLike(@Param("StoreName") String StoreName);
 }
