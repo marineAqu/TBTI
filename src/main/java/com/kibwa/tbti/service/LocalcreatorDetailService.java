06188e2b2eafe36227fde2269cc9b500bf1ce6ec
marineAqu <102001809+marineAqu@users.noreply.github.com>
2024-08-02 11:42:36 +0900

    [feat] postReview 메소드 추가 (2024.8.2 김도연)

--- src/main/java/com/kibwa/tbti/service/LocalcreatorDetailService.java
+++ src/main/java/com/kibwa/tbti/service/LocalcreatorDetailService.java
@@ -3,7 +3,9 @@ package com.kibwa.tbti.service;
 import com.kibwa.tbti.DTO.LocalcreatorDTO;
 import com.kibwa.tbti.DTO.LocalcreatorSearchProjection;
 import com.kibwa.tbti.entity.LocalcreatorEntity;
+import com.kibwa.tbti.entity.ReviewEntity;
 import com.kibwa.tbti.repository.LocalcreatorRepository;
+import com.kibwa.tbti.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import java.util.List;
@@ -20,6 +22,7 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class LocalcreatorDetailService {
     private final LocalcreatorRepository localcreatorRepository;
+    private final ReviewRepository reviewRepository;
 
     public LocalcreatorDTO SearchByStoreId(int store_id) {
         LocalcreatorEntity localcreatorEntity = localcreatorRepository.findByStoreId(store_id);
@@ -27,5 +30,13 @@ public class LocalcreatorDetailService {
         LocalcreatorDTO localcreatorDTO = LocalcreatorDTO.toLocalcreatorDTO(localcreatorEntity);
 
         return localcreatorDTO;
+    }
+
+    public void postReview(int store_id, String reviewContent, double starPoint) {
+        //TODO: 회원가입, 로그인 기능 추가 후 하드코딩된 memebrId 수정
+
+        ReviewEntity reviewEntity = ReviewEntity.toReviewEntity(reviewContent, starPoint, store_id, 1);
+
+        reviewRepository.save(reviewEntity);
     }
 }
