354dce5a23f0e9ba2fb17bdf43c5e5afb4af4376
marineAqu <102001809+marineaqu@users.noreply.github.com>
2024-08-10 18:12:37 +0900

    [add] 지도 연결 iframe 추가 (2024.8.15 조수현)

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -4,9 +4,15 @@ const Main = () => {
     return (
         <div>
             <h1>메인페이지</h1>
+            <iframe
+                src="map_data.html"
+                width="700"
+                height="1000"
+                title="index page"
+                style={{ border: 'none' }}
+            />
         </div>
     );
 };
 
 export default Main;
-
