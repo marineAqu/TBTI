ede56e80b4ad490ebd95fc489052513f5e1f3914
marineAqu <102001809+marineAqu@users.noreply.github.com>
2024-09-05 17:33:02 +0900

    [feat] 회원가입, 아이디와 닉네임 중복 검사 api 개발 (2024.9.5 김도연)

---
+++ src/main/java/com/kibwa/tbti/service/MemberService.java
@@ -0,0 +1,37 @@
+package com.kibwa.tbti.service;
+
+import com.kibwa.tbti.entity.MembersEntity;
+import com.kibwa.tbti.repository.MembersRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * 파일명: MemberService
+ * 작성자: 김도연
+ * 설명: 회원가입, 로그인, 로그아웃 등 서비스
+ **/
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MembersRepository membersRepository;
+
+    public void newMemberSave(String uid, String password, String username) {
+
+        MembersEntity membersEntity = new MembersEntity();
+        membersEntity.setUid(uid);
+        membersEntity.setPassword(password);
+        membersEntity.setUserName(username);
+        membersEntity.setIsActive('Y');
+
+        membersRepository.save(membersEntity);
+    }
+
+    public boolean uidDuplication(String uid) {
+        return membersRepository.existsByUid(uid);
+    }
+
+    public boolean userNameDuplication(String userName) {
+        return membersRepository.existsByUserName(userName);
+    }
+}
