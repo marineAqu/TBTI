f6b905c9affa1d155e8723afc6e9bef3c26f2b50
marineAqu <102001809+marineAqu@users.noreply.github.com>
2024-09-21 18:21:53 +0900

    [fix] 스프링 시큐리티 설정 (2024.9.21 김도연)

--- src/main/java/com/kibwa/tbti/config/SecurityConfig.java
+++ src/main/java/com/kibwa/tbti/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.kibwa.tbti.config;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -19,20 +18,17 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    //CORS 설정
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-
         config.setAllowCredentials(true);
         config.setAllowedOrigins(List.of("http://localhost:3000"));
-        config.setAllowedMethods(Arrays.asList("HEAD","POST","GET","DELETE","PUT"));
+        config.setAllowedMethods(Arrays.asList("HEAD", "POST", "GET", "DELETE", "PUT"));
         config.setAllowedHeaders(List.of("*"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
@@ -42,29 +38,14 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
         http.sessionManagement(session -> session
                 .sessionCreationPolicy(SessionCreationPolicy.ALWAYS));
 
-        //cors 실행
-        http.cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
-                    @Override
-                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-                        CorsConfiguration config = new CorsConfiguration();
-                        config.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
-                        config.setAllowedMethods(Collections.singletonList("*"));
-                        config.setAllowCredentials(true);
-                        config.setAllowedHeaders(Collections.singletonList("*"));
-                        config.setMaxAge(3600L);
-                        return config;
-                    }
-                }));
+        http.cors(corsCustomizer -> corsCustomizer.configurationSource(corsConfigurationSource()));
 
-        http.authorizeHttpRequests((authorizeRequests) -> authorizeRequests
-                        // 마이페이지는 인증된 사용자만 허용 (접근 시 로그인 페이지로 돌아감)
-                        //.requestMatchers("/mypage").authenticated()
+        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
                         .anyRequest().permitAll())
-                .csrf(AbstractHttpConfigurer::disable); // CSRF 보호 비활성화
+                .csrf(AbstractHttpConfigurer::disable);
 
         http.formLogin(login -> login
                 .loginPage("/login")
@@ -83,8 +64,8 @@ public class SecurityConfig {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder () {
-        return new BCryptPasswordEncoder(); //비밀번호 암호화
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
