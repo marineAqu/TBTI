922028e24d541e35d17d2728c8779aaa1b2d2919
marineAqu <102001809+marineAqu@users.noreply.github.com>
2024-09-30 10:31:46 +0900

    [chore] date 자료형 수정 (2024.9.30 김도연)

--- src/main/java/com/kibwa/tbti/DTO/ReviewDTO.java
+++ src/main/java/com/kibwa/tbti/DTO/ReviewDTO.java
@@ -2,7 +2,7 @@ package com.kibwa.tbti.DTO;
 
 import com.kibwa.tbti.entity.ReviewEntity;
 import lombok.*;
-import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 
 /**
  * 파일명: ReviewDTO
@@ -18,16 +18,17 @@ public class ReviewDTO {
     private int reviewId;
     private String reviewContent;
     private double rate;
-    private Timestamp createAt;
+    private String createAt;
     private String memberName;
 
     public static ReviewDTO toreviewDTO(ReviewEntity reviewEntity){
         ReviewDTO ReviewDTO = new ReviewDTO();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy년 MM월 dd일 작성");
 
         ReviewDTO.setReviewId(reviewEntity.getReviewId());
         ReviewDTO.setReviewContent(reviewEntity.getReviewContent());
         ReviewDTO.setRate(reviewEntity.getRate());
-        ReviewDTO.setCreateAt(reviewEntity.getCreateAt());
+        ReviewDTO.setCreateAt(simpleDateFormat.format(reviewEntity.getCreateAt()));
         ReviewDTO.setMemberName(reviewEntity.getMembers().getUserName());
 
         return ReviewDTO;
