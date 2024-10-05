0a7eaf3dba55dab8637493f09c74772fc02de3f1
marineAqu <102001809+marineAqu@users.noreply.github.com>
2024-10-05 15:57:10 +0900

    [fix] InformationRepository 자료형 수정 (2024.10.5 김도연)

--- src/main/java/com/kibwa/tbti/repository/InformationRepository.java
+++ src/main/java/com/kibwa/tbti/repository/InformationRepository.java
@@ -4,10 +4,12 @@ import com.kibwa.tbti.entity.InformationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 /**
  * 파일명: InformationRepository
  * 작성자: 김도연
  **/
 public interface InformationRepository extends JpaRepository<InformationEntity, Integer>{
-     InformationEntity findByStoreId(@Param("storeId") int storeId);
+     Optional<InformationEntity> findByStoreId(@Param("storeId") int storeId);
 }
