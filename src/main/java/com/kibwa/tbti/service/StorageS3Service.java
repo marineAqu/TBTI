39ebc7bb86f84cc08522ca0df8b3d4c688354735
marineAqu <102001809+marineAqu@users.noreply.github.com>
2024-11-19 17:16:20 +0900

    [fix] import 주석 추가 (2024.11.19 김도연)

--- src/main/java/com/kibwa/tbti/service/StorageS3Service.java
+++ src/main/java/com/kibwa/tbti/service/StorageS3Service.java
@@ -1,6 +1,6 @@
 package com.kibwa.tbti.service;
 
-import com.amazonaws.services.s3.AmazonS3;
+//import com.amazonaws.services.s3.AmazonS3;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
